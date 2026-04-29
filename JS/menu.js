8bff1a5681b07fc2e32da2cf0db1ddfda6195670
Vitor-Bruno-G-Santos <vitor_saske@hotmail.com>
2026-04-29 08:24:44 -0400

    task 03
    Modo escuro pronto e salvando na sessionStorage

--- JS/menu.js
+++ JS/menu.js
@@ -4,6 +4,8 @@ function modo_dia(){
     document.documentElement.style.setProperty('--cor-botao', '#FFC067');
     document.getElementById("dia").style.display = "none";
     document.getElementById("noite").style.display = "block";
+
+    sessionStorage.setItem("tema", "dia");
 }
 function modo_noite(){
     document.documentElement.style.setProperty('--fundo', '#1b2d53');
@@ -11,4 +13,16 @@ function modo_noite(){
     document.documentElement.style.setProperty('--cor-botao', 'rgb(98, 44, 122)');
     document.getElementById("noite").style.display = "none";
     document.getElementById("dia").style.display = "block";
+
+    sessionStorage.setItem("tema", "noite");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const temaSalvo = sessionStorage.getItem("tema");
+
+    if (temaSalvo == "dia") {
+        modo_dia();
+    } else {
+        modo_noite();
+    }
+});
